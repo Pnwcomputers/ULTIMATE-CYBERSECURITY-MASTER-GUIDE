599469e861a506ee2362c276e830c7daa7720f7c
Pacific Northwest Computers <142192730+Pnwcomputers@users.noreply.github.com>
2025-11-06 16:08:21 -0800

    Create if_else.sh

---
+++ Scripts/Bash/Examples/if_else.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# IF-ELSE-IF Loop Example
+# Usage: ./if_else_if.sh <file_or_directory>
+
+USER_INPUT="${1}"
+
+# Check if the user provided an argument
+if [[ -z "${USER_INPUT}" ]]; then
+  echo "You must provide an argument!"
+  exit 1
+fi
+
+# Check if the argument is of type file or directory
+if [[ -f "${USER_INPUT}" ]]; then
+ echo "${USER_INPUT} is a file."
+elif [[ -d "${USER_INPUT}" ]]; then
+  echo "${USER_INPUT} is a directory."
+else
+  echo "${USER_INPUT} is not a file or a directory."
+fi
