443089965fb0e9e84251bc9099872e5dfc2b0f18
Pacific Northwest Computers <142192730+Pnwcomputers@users.noreply.github.com>
2025-11-06 16:08:05 -0800

    Create user_input.sh

---
+++ Scripts/Bash/Examples/user_input.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# User Input Example
+# Takes input from the user and assigns to variables
+
+echo "What is your first name?"
+read -r firstname
+
+echo "What is your last name?"
+read -r lastname
+
+echo "Your first name is ${firstname} and your last name is ${lastname}"
