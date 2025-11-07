40cfdde00abeedfedf576c40eb648ff6167af71d
Pacific Northwest Computers <142192730+Pnwcomputers@users.noreply.github.com>
2025-11-06 16:01:31 -0800

    Create file_search.sh

---
+++ Scripts/Bash/file_search.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# File Search Example
+
+FILENAME="flow_control_with_if.txt"
+
+if [[ -f "${FILENAME}" ]]; then
+  echo "${FILENAME} already exists."
+  exit 1
+else
+  touch "${FILENAME}"
+fi
