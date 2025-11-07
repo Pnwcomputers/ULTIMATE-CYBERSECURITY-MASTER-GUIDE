063b27174a1c71d95830194bf07ab71a752ff1a6
Pacific Northwest Computers <142192730+Pnwcomputers@users.noreply.github.com>
2025-11-06 16:08:45 -0800

    Create while_loop.sh

---
+++ Scripts/Bash/Examples/while_loop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# While Loop Example
+# Runs until a signal file is created
+
+SIGNAL_TO_STOP_FILE="stoploop"
+
+# This loop will run until the file "stoploop" is available.
+while [[ ! -f "${SIGNAL_TO_STOP_FILE}" ]]; do
+  echo "The file ${SIGNAL_TO_STOP_FILE} does not yet exist..."
+  echo "Checking again in 2 seconds..."
+  sleep 2
+done
+
+echo "File was found! Exiting..."
