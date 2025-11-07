e92dd2904c77b1871d37190ee5174fcaf33bacd6
Pacific Northwest Computers <142192730+Pnwcomputers@users.noreply.github.com>
2025-11-06 16:09:01 -0800

    Create local_scope.sh

---
+++ Scripts/Bash/Examples/local_scope.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Local Scope Variable Example
+
+PUBLISHER="No Starch Press"
+
+print_name(){
+  local name
+  name="Black Hat Bash"
+  echo "${name} by ${PUBLISHER}"
+}
+
+print_name
+
+echo "Variable ${name} will not be printed here because it is a local variable."
